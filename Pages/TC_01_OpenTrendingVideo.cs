88f64b9ea8218064b905f9dcd3b5a55db2d534a9
Alina Kireeva <allina.kireeva88@gmail.com>
2019-05-31 12:10:00 +1200

    Add project files.

---
+++ Pages/TC_01_OpenTrendingVideo.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeTesting
+{
+    class TC_01_OpenTrendingVideo
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.youtube.com");
+            driver.Navigate().GoToUrl("https://www.youtube.com/feed/trending");
+            //IWebElement element = driver.FindElement(By.Id("endpoint"));
+            
+        }
+    }
+}
---
+++ UnitTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace YoutubeTesting
+{
+    public class FirstTest
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.youtube.com");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void FirstSeleniumTest()
+        {
+            var TrendingVideos = new TC_01_OpenTrendingVideo(_driver);
+
+        }
+
+    }
+}
