5586bb9737a2f404294c2ef334cd93925a50ce9a
Alina Kireeva <allina.kireeva88@gmail.com>
2019-05-31 13:00:34 +1200

    added private IWebDriver

--- Pages/TC_01_OpenTrendingVideo.cs
+++ Pages/TC_01_OpenTrendingVideo.cs
@@ -1,21 +1,18 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace YoutubeTesting
 {
-    class TC_01_OpenTrendingVideo
+    public class TC_01_OpenTrendingVideo
     {
+        private IWebDriver _driver;
+
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.youtube.com");
             driver.Navigate().GoToUrl("https://www.youtube.com/feed/trending");
-            //IWebElement element = driver.FindElement(By.Id("endpoint"));
+            IWebElement element = driver.FindElement(By.Id("endpoint"));
             
         }
     }
