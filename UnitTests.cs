1ea22f7e2734943bada0234c01e1179264f73d8b
Alina Kireeva <allina.kireeva88@gmail.com>
2019-06-01 12:39:27 +1200

    Added assertion to UnitTests.cs

--- UnitTests.cs
+++ UnitTests.cs
@@ -27,9 +27,19 @@ namespace YoutubeTesting
         {
            var trendingVideos = new YouTubeTrendingPage(_driver);
            trendingVideos.ClikcByText("Trending");
+
            var titleOnTrendingPage = trendingVideos.GetTitleByVideoIndex(1);
            var viewCountOnTrendingPage = trendingVideos.GetViewsByVideoIndex(1);
+
            trendingVideos.OpenTrendingVideoByIndex(1);
+            //var titleOnVideoPage = videoPage.GetTitle();
+            var titleOnVideoPage = trendingVideos.GetTitleByVideoIndex(1);
+            var viewCountOnVideoPage = trendingVideos.GetViewsByVideoIndex(1);
+
+            Assert.AreEqual(titleOnTrendingPage, titleOnVideoPage);
+            Assert.AreEqual(viewCountOnTrendingPage, viewCountOnVideoPage);
+
+
         }
 
     }
